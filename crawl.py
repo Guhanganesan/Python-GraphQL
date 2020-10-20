179cdcaf8a67b0c18a7944bc9919946f90d75b35
Guhan Ganesan <phpguhan@gmail.com>
2020-10-20 18:21:35 +0530

    Intial commit for crawl and graphql

---
+++ crawl.py
@@ -0,0 +1,13 @@
+import graphene
+import extraction
+import requests
+
+def extract(url):
+    html = requests.get(url).text
+    extracted = extraction.Extractor().extract(html, source_url=url)
+    return extracted
+
+data = extract('https://lethain.com/migrations/')
+print(data)
+
+
